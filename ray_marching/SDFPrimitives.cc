9fd02c413b364af26ceef6460fe3de2448a7410f
clement.gregoire <clement.gregoire@epita.fr>
2026-04-04 19:07:05 +0200

    #4 - add soft shadows, ambiant occlusion, plan to rendering

--- ray_marching/SDFPrimitives.cc
+++ ray_marching/SDFPrimitives.cc
@@ -3,3 +3,8 @@
 float SphereSDF::distance(const Vector3& p) const {
 	return (p - center).norm() - radius;
 }
+
+float PlaneSDF::distance(const Vector3& p) const {
+	return (p - point).dot(normal);
+}
+
--- ray_marching/SDFPrimitives.hh
+++ ray_marching/SDFPrimitives.hh
@@ -12,3 +12,15 @@ public:
 
 	float distance(const Vector3& p) const override;
 };
+
+class PlaneSDF: public SDF
+{
+public:
+	Vector3 point;
+	Vector3 normal;
+
+	PlaneSDF(const Vector3& point, const Vector3& normal): point(point), normal(normal.normalized()) {}
+	
+	float distance(const Vector3& p) const override;
+
+};
